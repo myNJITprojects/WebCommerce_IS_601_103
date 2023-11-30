7d2198eb75bf2aa156e29d2821e7ed8e24891735
Andres <softwareapgm@gmail.com>
2023-11-29 20:30:46 -0500

    Fix navBar

---
+++ htmlSetup.js
@@ -0,0 +1,46 @@
+const navBarCode = `
+<div class="NavB">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+            <!-- Image Logo -->
+            <a class="navbar-brand" href="#" style="color: blueviolet;">
+                <img src="./img/icons8-sweater-64.png" width="30" height="30" alt="Your Logo">
+                CozyGo
+            </a>
+
+            <!-- Clickable Menu -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./index.html">Home</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Browse</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+                
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./cart_page.html">
+                            <i class="fas fa-shopping-cart"></i> ❄️
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+</div>`
+
+
+function addNaBar(navBarCode){
+    document.getElementById("headerPage")
+        .insertAdjacentHTML("afterend", navBarCode);
+}
+
+function setupPage()
+{
+    addNaBar(navBarCode);
+
+}
