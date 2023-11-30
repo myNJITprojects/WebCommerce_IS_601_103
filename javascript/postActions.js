4389fffde1cfa2f94b7366d0f540682fa3effc1f
Andres <softwareapgm@gmail.com>
2023-11-29 19:31:23 -0500

    Modify post function

--- javascript/postActions.js
+++ javascript/postActions.js
@@ -1,30 +1,26 @@
 import fetch from 'node-fetch';
 
 async function postProduct(product, url) {
-    
-  const formData = product;
 
-  // Make a POST request using the Fetch API
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(formData),
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(newUserData => {
-      // Process the newly created user data
-      console.log('New User Data:', newUserData);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+    try{
+        // Make a POST request using the Fetch API
+        const response = await fetch(
+            url, 
+            { 
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify(product)
+            }
+        );
+
+        const data = await response.json(); 
+        console.log(data);
+
+    } catch(error){
+        console.error("Error POSTing products:", error);
+    }
+
+    
 }
 
 
@@ -34,9 +30,9 @@ var url = "http://34.229.91.169:8000/products/"
 
  // JSON for endpoint
  const formData = {
-    title: 'Gato',
+    title: 'Gato2',
     description: 'White and Black Cat',
-    price: 500,
+    price: 600,
 };
 
 
